adc5892ea8ea8059c020b3c9ecae94603a00c081
Muni Raghava <muniraghava.pallamala@gmail.com>
2022-01-19 10:37:08 +0530

    JDBC DML Commands

--- apache-maven-3.8.4/bin/demo/src/main/java/com/example/fktdmlqueries.java
+++ apache-maven-3.8.4/bin/demo/src/main/java/com/example/fktdmlqueries.java
@@ -1,10 +1,28 @@
 package com.example;
-import java.sql.*;
-import java.util.*;
 
+import java.sql.*;
 public class fktdmlqueries {
-    
-    public static void main(String[] args) {
-        
+
+    public static void main(String[] args) throws SQLException {
+        Connection cntn = DriverManager.getConnection("jdbc:mysql://localhost/flipkart", "root", "199812");
+        Statement st = cntn.createStatement();
+
+        // String query = "INSERT INTO CUSTOMER VALUES(3105,'HARI','NELLORE',400,5001)";
+        // String query1 = "INSERT INTO CUSTOMER VALUES(3103,'NAVEEN','MENAKURU',200,5002)";
+        // String query2 = "INSERT INTO CUSTOMER VALUES(3104,'BALAJI','NAIDUPETA',500,5003)";
+        // st.execute(query);
+        // st.execute(query1);
+        // st.execute(query2);
+
+        ResultSet rs = st.executeQuery("select * from CUSTOMER");
+
+        while (rs.next()) {
+            System.out.print("ID: " + rs.getInt("customer_id") + "\t");
+            System.out.print("Name: " + rs.getString("cust_name") + "\t");
+            System.out.print("City: " + rs.getString("city") + "\t");
+            System.out.print("Grade: " + rs.getInt("grade") + "\t");
+            System.out.print("Salesman ID: " + rs.getInt("salesman_id") + "\n");
+        }
+
     }
 }
